1873f254f9bf23628fb18205cdb5c636752eca87
genomics-geek <gonzalezma@email.chop.edu>
2019-10-05 14:44:41 -0400

    Added the welcome banner back in the home page

--- frontend/src/app/home/index.js
+++ frontend/src/app/home/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Grid } from 'semantic-ui-react'
 
-import AboutSection from './components/about-section'
-import RecentNews from './components/recent-news'
-// import WelcomeBanner from './components/welcome-banner'
+import AboutSection from './about-section'
+import RecentNews from './recent-news'
+import WelcomeBanner from './welcome-banner'
 
 import './index.scss'
 
@@ -11,7 +11,7 @@ import './index.scss'
 const Home = () => (
   <React.Fragment>
 
-    {/* <WelcomeBanner /> */}
+    <WelcomeBanner />
 
     <Grid padded className="about-section">
       <Grid.Column width={10}>
